cabed2d97e3a929ff645d5d157016a023d3709c9
johannes-baldinger <johannes.baldinger@googlemail.com>
2014-07-18 07:02:13 -0700

    Broken state

--- src/num2words.c
+++ src/num2words.c
@@ -18,34 +18,41 @@ static size_t append_string(char* buffer, const size_t length, const char* str) 
 }
 
 char* num_to_words(int num){
-  char* return_val="";
+  char* return_val="x";
   if (num >=0 && num <10 ) {
     return_val=EINER[num];
   } else if (num >=10 && num <20) { 
     return_val=ZEHN[num-10];
   } else if (num>=20 && num<60){
-    return_val=ZEHNER[num/10];
+    return_val="";
     if(num%10!=0){
       strcat(return_val, EINER[num%10]);
       strcat(return_val,STR_UND);
     }
+    return_val=strcat(return_val,ZEHNER[num/10]);
   }
   return return_val;
 }
 
-void fuzzy_time_to_words(int hours, int minutes, char* words, size_t length) {
+void fuzzy_time_to_words(int hours, int minutes, char* wordsHours, char* wordsMinutes, size_t length) {
   size_t remaining = length;
-  memset(words, 0, length);
-  remaining -= append_string(words, remaining, num_to_words(hours));
-  remaining -= append_string(words, remaining, " ");
-  remaining -= append_string(words, remaining, STR_UHR);
-  remaining -= append_string(words, remaining, " ");
+  size_t remainingHours = length;
+  
+  memset(wordsHours, 0, length);
+  memset(wordsMinutes, 0, length);
+  
+  remainingHours -= append_string(wordsHours, remainingHours, num_to_words(hours));
+  remainingHours -= append_string(wordsHours, remainingHours, " ");
+  remainingHours -= append_string(wordsHours, remainingHours, STR_UHR);
+  remainingHours -= append_string(wordsHours, remainingHours, " ");
   if (minutes>0){
-    remaining -= append_string(words, remaining, STR_UND);
-    remaining -= append_string(words, remaining, " ");
-    remaining -= append_string(words, remaining, num_to_words(minutes));
-    remaining -= append_string(words, remaining, " ");
-    remaining -= append_string(words, remaining,  STR_MINUTEN);
-  }  
+    remaining -= append_string(wordsMinutes, remaining, STR_UND);
+    remaining -= append_string(wordsMinutes, remaining, " ");
+    remaining -= append_string(wordsMinutes, remaining, num_to_words(minutes));
+    remaining -= append_string(wordsMinutes, remaining, " ");
+    remaining -= append_string(wordsMinutes, remaining,  STR_MINUTEN);
+  }  else {
+    remaining -= append_string(wordsMinutes, remaining, " ");
+  }
 }  
   
