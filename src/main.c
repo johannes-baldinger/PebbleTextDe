ab5457d08a11cce98a1925d790f61ab4a9bd1479
johannes-baldinger <johannes.baldinger@googlemail.com>
2014-07-19 04:30:55 -0700

    Bugs fixed.

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@ static struct CommonWordsData {
 
 static void update_time(struct tm* t) {
   fuzzy_time_to_words(t->tm_hour, t->tm_min,s_data.bufferHours, s_data.buffer, BUFFER_SIZE);
-  text_layer_set_text(s_data.label, strcat(s_data.buffer,"m"));
-  text_layer_set_text(s_data.labelBold, strcat(s_data.bufferHours,"h"));
+  text_layer_set_text(s_data.label, s_data.buffer);
+  text_layer_set_text(s_data.labelBold, s_data.bufferHours);
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -27,22 +27,22 @@ static void do_init(void) {
   window_stack_push(s_data.window, animated);
 
   window_set_background_color(s_data.window, GColorBlack);
-  GFont fontBold = fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD );
-  GFont font = fonts_get_system_font( FONT_KEY_GOTHIC_14 );
+  GFont fontBold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD );
+  GFont font = fonts_get_system_font( FONT_KEY_GOTHIC_28 );
   
 
   Layer *root_layer = window_get_root_layer(s_data.window);
   GRect frame = layer_get_frame(root_layer);
 
-  s_data.label = text_layer_create(GRect(0, 40, frame.size.w, frame.size.h - 40));
+  s_data.label = text_layer_create(GRect(0, 60, frame.size.w, frame.size.h - 60));
   text_layer_set_background_color(s_data.label, GColorBlack);
   text_layer_set_text_color(s_data.label, GColorWhite);
   text_layer_set_font(s_data.label, font);
   layer_add_child(root_layer, text_layer_get_layer(s_data.label));
 
-  s_data.labelBold = text_layer_create(GRect(0, 0, frame.size.w, 39));
-  text_layer_set_background_color(s_data.labelBold,GColorWhite );
-  text_layer_set_text_color(s_data.labelBold,GColorBlack );
+  s_data.labelBold = text_layer_create(GRect(0, 0, frame.size.w, 59));
+  text_layer_set_background_color(s_data.labelBold, GColorBlack );
+  text_layer_set_text_color(s_data.labelBold,GColorWhite );
   text_layer_set_font(s_data.labelBold, fontBold);
   layer_add_child(root_layer, text_layer_get_layer(s_data.labelBold));
   
